48e8713028b03bd3eb14a59d316eadd75134f0e9
Jennifer Tran <jennifertran@Jennifers-MacBook-Air.local>
2019-10-28 13:36:12 -0400

    average lumper fee function

--- trucksmartcontract.sol
+++ trucksmartcontract.sol
@@ -1,5 +1,7 @@
 pragma solidity >=0.4.22 <0.6.0;
 
+///usage of iToken, allowing users to lend truck drivers and earn interest
+
 ///Interface to connect Fulcrum and functions that will be used in the trucklending contract
 contract fulcrumInterface {
     function borrowInterestRate() public view returns (uint256);
@@ -8,7 +10,16 @@ contract fulcrumInterface {
     function mint(address receiver, uint256 depositAmount) external returns (uint256 mintAmount);
 }
 contract trucklending {
-    
+
+    uint lumperfee;
+    uint averagefee;
+
+    mapping (address => uint) public mappedUsers
+    address public truckaddresses;
+    uint arrayLength = addressIndices.length;
+    uint[] public totallumpers;
+    uint totallumpers;
+
     ///Connect Fulcrum Kovan Contract
     address fulcrumkovan = 0xF1C87dD61BF8a4e21978487e2705D52AA687F97E;
     fulcrumInterface fulcrumcontract = fulcrumInterface(fulcrumkovan);
@@ -23,6 +34,7 @@ contract trucklending {
     
     ///Struct for a warehouse
     struct warehouse {
+        string warehousename;
         string streetaddress;
         string city;
         string state;
@@ -31,8 +43,17 @@ contract trucklending {
     }
 
     ///Loan based on lumper fee inputted
-    function lumperpayment(uint lumperfee) {
-        mint(msg.sender, lumperfee);
+    function lumperpayment(uint lumperfee) public {
+        mint(msg.sender, lumperfee); ///using mint function
         return lumperfee;
+    }
+    
+    ///find the average of lumperfee
+    function findaveragefee(address warehouseaddress, uint lumpers) public {
+        lumperfee.push(lumpers);
+        for (uint i = 0; i < arrayLength; i++) {
+            totallumpers = lumpers[i] + lumpers[i + 1];
+        }
+        return totallumpers/(arrayLength);
     }
 }
